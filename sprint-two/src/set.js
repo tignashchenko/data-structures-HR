90ad8cc317e30b6fd542eed801fafdc04b6d0468
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-20 19:34:14 -0700

    Implement set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!_.contains(this._storage, item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item) {
+  _.each(this._storage, (element, index) => {
+    if (element === item) {
+      this._storage.splice(index, 1);
+    }
+  });
 };
 
 /*
